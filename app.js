02c3225fbbd439cb88e87fd8e3a956164d9546c2
muhammadhashirsafdar <139388035+muhammadhashirsafdar@users.noreply.github.com>
2023-10-20 00:25:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,207 @@
+
+
+                    // ! function
+
+
+
+// ?  Question 1
+
+
+
+// function date() {
+//     var a = new Date();
+//     document.write(a);
+// };
+// date();
+
+
+
+// ?  Question 2
+
+
+
+// function user() {
+//     var a = "Muhammad ";
+//     var b = "Hashir";
+//     var c = a + b ;
+//     document.write("Hello"," ",c);
+// };
+//  user();
+
+
+
+// ?  Question 3
+
+
+
+// function add() {
+//     var a = +prompt("Enter a number ");
+//     var b = +prompt("Enter second number ");
+//     var c = a + b ;
+//     document.write("addition of given number is : "," ",c);
+// };
+// add();
+
+
+
+// ?  Question 4
+
+
+
+// function calculator() {
+//     var a = +prompt("Enter a number");
+//     var b = +prompt("Enter second number");
+//     var c = prompt("Enter desired operator");
+    
+//     if(c=="+"){
+//         document.write(a+b);
+//     }
+//     else if (c=="-"){
+//         document.write(a-b);
+//     }
+//     else if (c=="*"){
+//         document.write(a*b);
+//     }
+//     else if (c=="/"){
+//         document.write(a / b);
+//     }
+//     else if (c=="%"){
+//         document.write(a % b);
+//     }
+// }
+// calculator();
+
+
+
+// ?  Question 5
+
+
+
+// function square(number) {
+//     document.write ( number*number)
+// }
+// square(6);
+
+
+
+// ?  Question 6
+
+
+
+// var n = 7;
+// answer = factorial(n)
+// document.write(" "," ","Factorial of " + n  );
+// function factorial(n){
+//     var answer = 1;
+//     if (n == 0 || n == 1){
+//       document.write (answer);
+//     }
+//     else if(n > 1){
+//       for(var i = n; i >= 1; i--){
+//         answer = answer * i;
+//       }
+//       document.write (answer);
+//     }
+//     else{
+//       document.write ("number has to be positive.")
+//     }  
+//   };
+
+
+
+// ?  Question 7
+
+
+
+// function num(){
+//     var a = +prompt("enter starting number");
+//     var b = +prompt("enter ending number");
+//     for (i=a ; i<b ; i++){
+//     console.count()
+//     };
+// };
+// num();
+
+
+
+// ?  Question 8
+
+
+
+// function hyp(){
+
+//     document.write ("hypotenous = "," ",base*base,height*height);
+// };
+// var base = +prompt("give value to base");
+// var height = +prompt("give value to height");
+// hyp();
+
+
+
+// ?  Question 9
+
+
+
+// function area(width,height){
+//     document.write(width*height);
+// };
+// area(5,8);
+
+
+
+// ?  Question 10
+
+
+
+// function checkPalindrome(string) {
+
+    
+//     var len = string.length;
+
+   
+//     for (var i = 0; i < len / 2; i++) {
+
+       
+//         if (string[i] !== string[len - 1 - i]) {
+//             return 'It is not a palindrome';
+//         }
+//     }
+//      return 'It is a palindrome';
+// }
+
+// var string = prompt('Enter a string: ');
+
+
+// var value = checkPalindrome(string);
+
+// document.write(value);
+
+
+
+// ?  Question 12
+
+
+
+// function getLongestWord(str){
+//     let words = str.split(' ');
+//     let maxLength = 0;
+//     let longestWord = '';
+  
+//     for (let i = 0; i < words.length; i++) {
+//       if (words[i].length > maxLength) {
+//         maxLength = words[i].length;
+//         longestWord = words[i];
+//       }
+//     }
+  
+//     console.log(maxLength);
+//     console.log(longestWord);
+  
+//   };
+//   getLongestWord("Muhhammad hashir");
+
+
+
+// ?  Question 11
+// ?  Question 13
+// ?  Question 14
